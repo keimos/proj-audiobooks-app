1aa39beb14bbb3feb89088864d4c5ae9e01faf5e
keimos <keithmosleyjr@gmail.com>
2025-04-07 17:34:49 -0400

    Added Setup Router

--- tests/main_test.go
+++ tests/main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestMain(m *testing.M) {
@@ -15,4 +17,12 @@ func TestMain(m *testing.M) {
 	code := m.Run()
 	os.Remove("test.db")
 	os.Exit(code)
+}
+
+func setupRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/upload", UploadHandler).Methods("GET")
+	router.HandleFunc("/audiobook/{id}", GetAudiobookHandler).Methods("GET")
+	router.HandleFunc("/audiobooks", ListAudiobooksHandler).Methods("POST")
+	return router
 }
